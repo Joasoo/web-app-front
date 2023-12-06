783173b6027ffa57402e314d311add04694c3944
majoas <majoas@taltech.ee>
2023-12-06 00:23:37 +0000

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://127.0.01',
+            target: 'http://localhost/',
             changeOrigin: true,
         })
     )
